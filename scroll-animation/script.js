bde0aab6611735c4b92f1ddcab47c4ee6a8d18b8
Theo Bondaz <bondaz.theo@gmail.com>
2022-05-02 18:12:53 +0200

    wip on scroll-animation

---
+++ scroll-animation/script.js
@@ -0,0 +1,25 @@
+const boxes = document.querySelectorAll('.box');
+
+
+window.addEventListener('scroll', checkBoxes);
+
+// checkBoxes()
+
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 2;
+  console.log(triggerBottom);
+
+  boxes.forEach((box) => {
+    const boxTop  = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      console.log(boxTop);
+      box.classList.add('active')
+    } else {
+      // console.log(boxTop);
+      box.classList.add('remove');
+    }
+  })
+
+}
