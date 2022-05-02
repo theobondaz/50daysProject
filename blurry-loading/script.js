abaa707bc67504002e7d384600f7779177778723
Theo Bondaz <bondaz.theo@gmail.com>
2022-05-02 15:30:53 +0200

    wip blurry loading

---
+++ blurry-loading/script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector("h1");
+const body = document.querySelector("body");
+const img = document.querySelector("img");
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  img.classList.add('active');
+})
